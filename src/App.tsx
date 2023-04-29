7753ecfea4a822aa2bd1e9b92f427107dfaaa5da
Linus Romland <hello@linusromland.com>
2023-04-29 11:46:35 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(11)
+  const [count, setCount] = useState(1)
 
   return (
     <div className="App">
